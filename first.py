271ff9483d310ffd8216764147fa61ef3ee0a874
lukechamley <lkchamley@gmail.com>
2023-05-03 15:08:10 -0600

    lil change

--- first.py
+++ first.py
@@ -5,10 +5,10 @@ name = input("Enter your name weary traveller: ")
 occupation= input("And occupation: ")
 print("Hello "+name+"! I hear it's tough being a "+occupation+" these days")
 
+# maybe expand these inputs/prints to make a short little madlibs game.
+
 # make my summary a string assigned to a variable then make a few simple functions
 # that will make the text all uppercase, all lowercase, and/or a different font/color 
-
-# maybe include the input functions above^ before displaying the summary so that it seems a little cooler.
 
 # Simple Calculator Function
 num1= input("Enter a number:")
@@ -17,3 +17,4 @@ num2= input("Enter another number:")
 result= float(num1) + float(num2)
 
 print(result)
+
