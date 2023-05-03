f3e5844ca66dc083791bcbbbe694c267c4f0db4b
lukechamley <lkchamley@gmail.com>
2023-05-03 14:51:54 -0600

    input testing

--- first.py
+++ first.py
@@ -8,5 +8,9 @@ my_age="22"
 print ("There was once a man named "+ my_name + ", ")
 print ("he was "+ my_age + " years old" )
 
+name = input("Enter your name weary traveller: ")
+occupation= input("And occupation: ")
+print("Hello "+name+"! I hear it's tough being a "+occupation+" these days")
+
 # make my summary a string assigned to a variable then make a few simple functions
 # that will make the text all uppercase, all lowercase, and/or a different font/color 
