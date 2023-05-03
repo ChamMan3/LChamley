70efbdd4c56317722e35a6478e00c1d75412684d
lukechamley <lkchamley@gmail.com>
2023-05-03 14:58:25 -0600

    simple calculator

--- first.py
+++ first.py
@@ -1,12 +1,5 @@
 from math import *
 
-msg = "hello world"
-print(msg)
-
-my_name="Luke"
-my_age="22"
-print ("There was once a man named "+ my_name + ", ")
-print ("he was "+ my_age + " years old" )
 
 name = input("Enter your name weary traveller: ")
 occupation= input("And occupation: ")
@@ -14,3 +7,13 @@ print("Hello "+name+"! I hear it's tough being a "+occupation+" these days")
 
 # make my summary a string assigned to a variable then make a few simple functions
 # that will make the text all uppercase, all lowercase, and/or a different font/color 
+
+# maybe include the input functions above^ before displaying the summary so that it seems a little cooler.
+
+# Simple Calculator Function
+num1= input("Enter a number:")
+num2= input("Enter another number:")
+
+result= float(num1) + float(num2)
+
+print(result)
