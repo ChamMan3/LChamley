7fa92b26fdaf95a4e66045512cf53d7e2735cb99
lukechamley <lkchamley@gmail.com>
2023-05-03 14:23:36 -0600

    notes

--- first.py
+++ first.py
@@ -5,3 +5,6 @@ my_name="Luke"
 my_age="22"
 print ("There was once a man named "+ my_name + ", ")
 print ("he was "+ my_age + " years old" )
+
+# make my summary a string assigned to a variable then make a few simple functions
+# that will make the text all uppercase, all lowercase, and/or a different font/color 
